383f826f1b3d5fe8cc68a0ad36ba7ad89aa2e284
Joao Vitor Garcia <jvdasilvaeirasgarcia@gmail.com>
2022-07-08 15:44:31 -0300

    Feat: starting do encrypt a file

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import crypto from 'crypto-js';
 
 import axios from 'axios';
 
-// const FILE_LINK = 'https://plin-condominios.s3.sa-east-1.amazonaws.com/44f0f06be281c605c7af2ed65cec48e1-RINGFIX_AMOSTRA.gcode'
+const FILE_CLEAR_LINK = 'https://plin-condominios.s3.sa-east-1.amazonaws.com/44f0f06be281c605c7af2ed65cec48e1-RINGFIX_AMOSTRA.gcode'
 const FILE_LINK = 'https://test-plin-condominiums-bucket.s3.sa-east-1.amazonaws.com/88f9e85aa462348f42b2a3ef9f3eced5-00c111b282be_RINGFIX_RINGFIX_M.fixit'
 const FIXIT_FILE_KEY = '7F24D27FAD171AF2';
 
@@ -91,6 +91,73 @@ function App() {
     }
   }
 
+  async function cryptFile() {
+    try {
+      const root = await navigator.storage.getDirectory();
+      const dirHandle = await root.getDirectoryHandle('tmpFixitFolder', { create: true });
+      const fileHandle = await dirHandle.getFileHandle('ringClear.fixit', { create: true });
+      const writable = await fileHandle.createWritable();
+      const response = await fetch(FILE_CLEAR_LINK);
+    
+      await response.body?.pipeTo(writable);
+      const file = await fileHandle.getFile();
+
+      const writableStream = new WritableStream({
+        write: (chunk) => {
+          const ciphertext = crypto.AES.encrypt(chunk, FIXIT_FILE_KEY).toString();
+
+          // const bytes  = crypto.AES.decrypt(ciphertext, FIXIT_FILE_KEY);
+          // const originalText = bytes.toString(crypto.enc.Utf8);
+
+          // console.log(ciphertext);
+          // console.log(originalText);
+        },
+      });
+
+      await file
+        .stream()
+        .pipeThrough(new TextDecoderStream())
+        .pipeTo(writableStream);
+      
+      console.log(file);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function decryptFile() {
+    try {
+      const root = await navigator.storage.getDirectory();
+      const dirHandle = await root.getDirectoryHandle('tmpFixitFolder', { create: false });
+      const fileHandle = await dirHandle.getFileHandle('ringClear.fixit', { create: false });
+
+      const file = await fileHandle.getFile();
+
+      const writableStream = new WritableStream({
+        write: (chunk) => {
+          // const ciphertext = crypto.AES.encrypt(chunk, FIXIT_FILE_KEY).toString();
+
+          console.log(chunk);
+
+          // const bytes  = crypto.AES.decrypt(ciphertext, FIXIT_FILE_KEY);
+          // const originalText = bytes.toString(crypto.enc.Utf8);
+
+          // console.log(ciphertext);
+          // console.log(originalText);
+        },
+      });
+
+      await file
+        .stream()
+        .pipeThrough(new TextDecoderStream())
+        .pipeTo(writableStream);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className='app_container'>
       <h1>File system</h1>
@@ -101,6 +168,14 @@ function App() {
 
       <button type="button" className="file_download" onClick={handleGetFile}>
         Carregar arquivo
+      </button>
+
+      <button type="button" className="file_download" onClick={cryptFile}>
+        Encriptogravar arquivo
+      </button>
+
+      <button type="button" className="file_download" onClick={decryptFile}>
+        Descriptografar arquivo
       </button>
     </div>
   )
